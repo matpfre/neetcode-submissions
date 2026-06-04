6c56a550159375a8939f86bc15c47f78e12c5066
mateus <137237205+matpfre@users.noreply.github.com>
2026-06-04 19:39:51 -0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length) return false;
+
+        Dictionary<char, int> count = new Dictionary<char, int>();
+        foreach (char c in s) {
+            if(count.ContainsKey(c))
+                count[c]++;
+            else
+                count[c] = 1;
+        }
+        foreach (char c in t)   {
+            if (!count.ContainsKey(c) || count[c] == 0)  return false;
+            count[c]--;
+        }
+        return true;
+    }
+
+}
